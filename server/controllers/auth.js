6e9e471070a8b8336254f03fe5a73b9dfbf7e2f7
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-01-27 17:16:03 +0600

    add login and me

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -55,3 +55,65 @@ export const register=async(req, res)=>{
 	
 }
 }
+
+
+export const login = async (req, res)=>{
+	try{
+		const { username, password } = req.body
+		const user = await User.findOne({username})
+		if(!user){
+			return res.json({
+				message:"Такого пользователя не существует"
+			})
+		}
+		const isPasswordCorrect = await bcrypt.compare(password, user.password)
+		if(!isPasswordCorrect){
+			return res.json({
+				message:"Неверный пароль"
+			})
+		}
+
+		const token = jwt.sign(
+			{
+			id:user._id,
+		},
+		process.env.JWT_SECRET,
+		{expiresIn:'30d'},
+		)
+		res.json({
+			token, 
+			user,
+			message:"Вы вошли в систему"
+
+		})
+	}catch(e){
+		res.json({ message: 'Ошибка при авторизации.' })
+	}
+}
+
+export const getMe = async (req, res)=>{
+	try {
+		const user = await User.findById(req.userId)
+
+		if (!user) {
+				return res.json({
+						message: 'Такого пользователя не существует.',
+				})
+		}
+
+		const token = jwt.sign(
+				{
+						id: user._id,
+				},
+				process.env.JWT_SECRET,
+				{ expiresIn: '30d' },
+		)
+
+		res.json({
+				user,
+				token,
+		})
+} catch (error) {
+		res.json({ message: 'Нет доступа.' })
+}
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.js";
+import { getMe, login, register } from "../controllers/auth.js";
 
 
 
 const router = new Router()
 
 router.post('/register', register)
+router.post('/login', login)
+router.get('/me', getMe)
 
 
 
